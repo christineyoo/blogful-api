1bbf91d636e738c0120d7bc7fa12f4d044f2592c
Christine Yoo <christinecho.s@gmail.com>
2021-04-15 21:11:14 -0700

    made a test and endpt for GET /articles/:article_id

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -61,5 +61,13 @@ describe.only('Articles Endpoints', function () {
     it('GET /articles responds with 200 and all of the articles', () => {
       return supertest(app).get('/articles').expect(200, testArticles);
     });
+
+    it('GET /articles/:article_id responds with 200 and the specified article', () => {
+      const articleId = 2;
+      const expectedArticle = testArticles[articleId - 1];
+      return supertest(app)
+        .get(`/articles/${articleId}`)
+        .expect(200, expectedArticle);
+    });
   });
 });
