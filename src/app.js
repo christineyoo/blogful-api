9b0f3724a44105a9a8ded2cf4c3d8843dfb68b44
Christine Yoo <christinecho.s@gmail.com>
2021-04-30 16:26:48 -0700

    required router for users-router

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const articlesRouter = require('./articles/articles-router');
+const usersRouter = require('./users/users-router');
 
 const app = express();
 
@@ -15,10 +16,10 @@ app.use(helmet());
 app.use(cors());
 
 app.use('/api/articles', articlesRouter);
+app.user('/api/users', usersRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
-  
 });
 
 app.use(function errorHandler(error, req, res, next) {
