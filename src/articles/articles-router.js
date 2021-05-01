cba7bab2ecd65d4dda85c7cc190bf93dbe5e6a8a
Christine Yoo <christinecho.s@gmail.com>
2021-04-30 18:01:46 -0700

    updated code to include an author when new article is created

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -11,7 +11,8 @@ const serializeArticle = (article) => ({
   style: article.style,
   title: xss(article.title),
   content: xss(article.content),
-  date_published: article.date_published
+  date_published: article.date_published,
+  author: article.author
 });
 
 articlesRouter
@@ -25,17 +26,15 @@ articlesRouter
       .catch(next);
   })
   .post(jsonParser, (req, res, next) => {
-    const { title, content, style } = req.body;
+    const { title, content, style, author } = req.body;
     const newArticle = { title, content, style };
 
-    for (const [key, value] of Object.entries(newArticle)) {
-      if (value == null) {
+    for (const [key, value] of Object.entries(newArticle))
+      if (value == null)
         return res.status(400).json({
           error: { message: `Missing '${key}' in request body` }
         });
-      }
-    }
-
+    newArticle.author = author;
     ArticlesService.insertArticle(req.app.get('db'), newArticle)
       .then((article) => {
         res
